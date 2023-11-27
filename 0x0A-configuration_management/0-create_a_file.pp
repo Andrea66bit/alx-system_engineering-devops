de9f7d1637c47f38f38c73ba9a11dd3793260c30
Andrea66bit <njabuloandrea@gmail.com>
2023-11-27 12:53:29 +0000

    /universe amd64 Packages

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# create a file in /tmp with specific requirements
-
-file { 'school':
-  path    => '/tmp/school',
+# create a file
+file {
+  '/tmp/school':
   mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+  owner   => www-data,
+  group   => www-data,
   content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-# create a manifest that kills a process named killmenow
+# kill process
 
-exec { 'pkill':
-  command  => 'pkill -f killmenow',
-  provider => 'shell',
+exec { 'killmenow':
+  command => '/bin/pkill killmenow',
 }
