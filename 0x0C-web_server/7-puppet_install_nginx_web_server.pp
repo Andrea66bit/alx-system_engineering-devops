dedb29b7159a5de6da8cdf5d45414ffef498b31d
Andrea66bit <njabuloandrea@gmail.com>
2023-11-28 12:11:00 +0000

    install_nginx_web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,49 +1,50 @@
-# installs and configures Nginx server
-
-# update apt-get first
-exec { 'apt-get update':
-  command => 'sudo apt-get update',
+# add stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# install Nginx package
-package { 'install nginx':
-  ensure => 'installed',
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# adjust the firewall
+# install nginx
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+# allow HTTP
 exec { 'allow HTTP':
-  command => "sudo ufw allow 'Nginx HTTP'",
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
+
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# file permissions and ownership
-exec { 'change file permissions':
-  command => 'sudo chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# change index file
+# create index file
 file { '/var/www/html/index.html':
-  ensure  => 'present',
   content => "Hello World!\n",
 }
 
-# create custom 404 page
-file { '/var/www/html/custom_404.html':
+# create index file
+file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
 }
 
-# configure nginx:
-#   lsiten on port 80
-#   redirect /redirect_me to 301
-#   custom 404 page
+# add redirection and error page
 file { 'Nginx default config file':
   ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
   content =>
 "server {
-	listen 80 default_server;
+        listen 80 default_server;
         listen [::]:80 default_server;
                root /var/www/html;
         # Add index.php to the list if you are using PHP
@@ -54,24 +55,24 @@ file { 'Nginx default config file':
                 # as directory, then fall back to displaying a 404.
                 try_files \$uri \$uri/ =404;
         }
-        error_page 404 /custom_404.html;
-        location  /custom_404.html {
+        error_page 404 /404.html;
+        location  /404.html {
             internal;
         }
         
-        location /redirect_me {
-		 rewrite ^/redirect_me$ https://github.com/Beldine-Moturi permanent;
-	}
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
 }
 ",
 }
-
 # restart nginx
-exec { 'restart nginx':
-  command => 'sudo service nginx restart',
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-# start nginx
+# start service nginx
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
